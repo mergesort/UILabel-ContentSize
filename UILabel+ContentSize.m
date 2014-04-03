021b28d4559b4e6ad646540ee766ff86d958518d
Joe Fabisevich <Joe@fabisevi.ch>
2014-04-02 22:45:53 -0400

    Utilize the label font into measuring expected content size

--- UILabel+ContentSize.m
+++ UILabel+ContentSize.m
@@ -13,17 +13,12 @@
 
 - (CGSize)contentSize
 {
-    NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
     paragraphStyle.lineBreakMode = self.lineBreakMode;
     paragraphStyle.alignment = self.textAlignment;
     
-    NSDictionary * attributes = @{
-                                   NSFontAttributeName : self.font,
-                                   NSParagraphStyleAttributeName : paragraphStyle
-                                 };
-
-    CGSize contentSize = [self.text boundingRectWithSize:self.frame.size options:(NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading) attributes:attributes context:nil].size;
-    return contentSize;
+    CGRect contentFrame = [self.text boundingRectWithSize:CGSizeMake(CGRectGetWidth(self.bounds), MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading attributes:@{ NSFontAttributeName : self.font } context:nil];
+    return contentFrame.size;
 }
 
 @end
